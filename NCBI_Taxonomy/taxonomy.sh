b91c7fdf95c0d7bc8521f297b83362bdc76745e4
isrusin <is_rusinov@mail.ru>
2017-03-25 00:21:51 +0300

    [Taxonomy] Rename scripts and clarify progress messages.
    * Rename python scripts.
    * Improve syntax of the pipeline script and clarify progress messages.

--- NCBI_Taxonomy/taxonomy.sh
+++ NCBI_Taxonomy/taxonomy.sh
@@ -6,12 +6,11 @@ date "+%D %T"
 
 wget -nv -NP "$WDIR" ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
 
-printf "1_get_taxon_ranks.py ...\n"
-time $WDIR/1_get_taxon_ranks.py -o "$WDIR/taxonomy.tab" \
-        "$WDIR/taxdump.tar.gz"
+printf "\nParsing NCBI taxdump archive ...\n"
+time $WDIR/parse_taxdump.py "$WDIR/taxdump.tar.gz" -o "$WDIR/taxonomy.tab"
 
-printf "2_extract_by_root_taxid.py ...\n"
-time $WDIR/2_extract_by_root_taxid.py 2 2157 \
-        -i "$WDIR/taxonomy.tab" -o "$WDIR/taxonomy_prokaryotes.tab"
+printf "\nExtracting prokaryotic subtree ...\n"
+time $WDIR/extract_subtree.py 2 2157 \
+    -i "$WDIR/taxonomy.tab" -o "$WDIR/taxonomy_prokaryotes.tab"
 
 date "+%D %T"
