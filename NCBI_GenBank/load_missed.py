88058a058207216383a394832c9526a168e34044
isrusin <is_rusinov@mail.ru>
2017-03-26 19:31:20 +0300

    [GenBank] Update the main script.
    * Add functions to check working folder, to get the list of missed
    ACvs, to send a request to NCBI efetch, to update the .acvs dump file.
    * Make some PEP changes.
    * Add module docsting, change CLI to match the current functionality.
    * Change the shebank to call env.
    * Correct the parser and Fasta headers to fit new NCBI format
    specifications.
    * Update request arguments and headers to get valid responce from
    the NCBI.

--- NCBI_GenBank/load_missed.py
+++ NCBI_GenBank/load_missed.py
@@ -1,67 +1,140 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
-import argparse as ap
+"""Load missed NCBI Nucleotide entries through efetch request.
+
+* Get list of missed ACs.
+* Send a request to Entrez Direct Fetch util.
+* Split th uploaded GenBank Flatfile by entries, save sequences in
+separate Fasta files; gzip everything.
+"""
+
+import argparse
 import gzip
-import update_acvs_dump
+import os
+import socket
+import sys
+import urllib2
+import urllib
 
-def parse(ingbk, wdir):
-    gbk_file = "%s/gbk/%s.gbk.gz"
-    fasta_file = "%s/fasta/%s.fasta.gz"
+
+def check_wdir(wdir):
+    gbk_dir = "%s/gbk" % wdir
+    if not os.path.exists(gbk_dir):
+        os.makedirs(gbk_dir)
+    fasta_dir = "%s/fasta" % wdir
+    if not os.path.exists(fasta_dir):
+        os.makedirs(fasta_dir)
+    return os.path.isdir(gbk_dir) and os.path.isdir(fasta_dir)
+
+
+def get_missed(inacvs, wdir, verbose=True):
+    acvs = set()
+    with inacvs:
+        acvs.update(inacvs.read().split())
+    try:
+        with open("%s/.acvs" % wdir) as dump:
+            acvs.difference_update(dump.read().split())
+    except IOError:
+        pass
+    if verbose:
+        print "%d ACvs are missed." % len(acvs)
+    return sorted(acvs)
+
+
+def request_efetch(acvs):
+    socket.setdefaulttimeout(300)
+    email = "%s@%s" % (os.getlogin(), socket.gethostname())
+    post_args = urllib.urlencode({
+        "db": "nuccore", "rettype": "gbwithparts", "retmode": "text",
+        "email": email, "id": ",".join(acvs)
+    })
+    req = urllib2.Request(
+        "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi",
+        post_args, {"Content-Type": "application/x-www-form-urlencoded",
+                    "User-Agent": "Firefox"}
+    )
+    return urllib2.urlopen(req)
+
+
+def parse_gbk(ingbk, wdir, verbose=True):
+    gbk_path = "%s/gbk/%s.gbk.gz"
+    fasta_path = "%s/fasta/%s.fasta.gz"
     count = 0
     while True:
         count += 1
-        title = ""
+        header = ""
         acv = ""
-        gi = ""
         de = ""
-        date = ""
         for line in ingbk:
-            title += line
-            if line.startswith("LOCUS"):
-                date = line.split()[-1]
-            elif line.startswith("DEFINITION") or line.startswith(" "):
+            header += line
+            if line.startswith("DEFINITION") or line.startswith(" "):
                 de += line[11:-1] # + leading " ", - trailing "\n"
             elif line.startswith("VERSION"):
-                acv, gi = line.split()[1:]
-                gi = gi[3:]
+                acv = line.split()[1]
                 break
         else:
             break # EOF
-        with gzip.open(gbk_file % (wdir, acv), "w", 5) as ougbk:
-            ougbk.write(title)
+        with gzip.open(gbk_path % (wdir, acv), "w", 5) as ougbk:
+            ougbk.write(header)
             for line in ingbk:
                 ougbk.write(line)
                 if line.startswith("ORIGIN"):
                     ougbk.write("//\n")
                     break
                 if line.startswith("//"):
-                    print "%s record has no sequence!\n" % acv
+                    if verbose:
+                        print "%s record has no sequence!\n" % acv
                     break
-        with gzip.open(fasta_file % (wdir, acv), "w", 5) as oufasta:
-            oufasta.write(">gi|%s|gb|%s%s [%s]\n" % (gi, acv, de, date))
+        with gzip.open(fasta_path % (wdir, acv), "w", 5) as oufasta:
+            oufasta.write(">%s%s\n" % (acv, de))
             for line in ingbk:
                 if line.startswith("//"):
                     continue
                 if line == "\n":
                     break
                 oufasta.write(line[10:].replace(" ", ""))
-        print "%d\t%s was loaded." % (count, acv)
-    update_acvs_dump.update(wdir)
+        if verbose:
+            print "%d\t%s was loaded." % (count, acv)
+
+
+def update_dump(wdir, verbose=True):
+    gacvs = set()
+    for name in os.listdir(wdir + "/gbk/"):
+        gacvs.add(name.split(".gbk")[0])
+    facvs = set()
+    for name in os.listdir(wdir + "/fasta/"):
+        facvs.add(name.split(".fasta")[0])
+    acvs = list(gacvs.intersection(facvs))
+    acvs.sort()
+    with open(wdir + "/.acvs", "w") as ouacvs:
+        ouacvs.write("\n".join(acvs) + "\n")
+    if verbose:
+        print "%d records in the database." % len(acvs)
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        description="""Load missed NCBI Nucleotide entries."""
+    )
+    parser.add_argument(
+        "inacvs", metavar="LIST", type=argparse.FileType("r"),
+        help="input file with whitespace-separated list of ACs"
+    )
+    parser.add_argument(
+        "-s", "--source", dest="wdir", metavar="DIR", default=".",
+        help="working folder"
+    )
+    args = parser.parse_args(argv)
+    wdir = args.wdir
+    if not check_wdir(wdir):
+        parser.error("bad working folder (-s option)")
+    acvs = get_missed(args.inacvs, wdir)
+    resp = request_efetch(acvs)
+    parse_gbk(resp, wdir)
+    resp.close()
+    update_dump(wdir)
+
 
 if __name__ == "__main__":
-    parser = ap.ArgumentParser(
-            description="Gets .gbk file with multiple records and " +
-            "splits it into .gbk (without sequence) and .fasta files."
-            )
-    parser.add_argument(
-            "ingbk", metavar="input.gbk", type=ap.FileType("r"),
-            help="input .gbk file with multiple records"
-            )
-    parser.add_argument(
-            "-s", "--source", dest="wdir", default=".",
-            help="working folder"
-            )
-    args = parser.parse_args()
-    with args.ingbk as ingbk:
-        parse(ingbk, args.wdir)
+    sys.exit(main())
 
