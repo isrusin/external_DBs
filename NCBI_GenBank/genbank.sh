30d7a957244f3b2e919023490b90bf357cda057e
isrusin <is_rusinov@mail.ru>
2017-03-26 19:30:11 +0300

    [GenBank] Add step of getting sequence length to the pipeline.

--- NCBI_GenBank/genbank.sh
+++ NCBI_GenBank/genbank.sh
@@ -14,4 +14,8 @@ time $WDIR/load_missed.py "$GDIR/plasmids.acvs" -s "$WDIR"
 printf "Downloading chromosomes ...\n"
 time $WDIR/load_missed.py "$GDIR/chromosomes.acvs" -s "$WDIR"
 
+printf "Getting lengths ...\n"
+time $WDIR/get_lengths.py "$WDIR/gbk/{}.gbk.gz" -l "$WDIR/.acvs"
+    -o "$WDIR/acv2len.dct"
+
 date "+%D %T"
