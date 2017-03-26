75f514aae2c0afa7dc68c60c68714328b70a6580
isrusin <is_rusinov@mail.ru>
2017-03-26 19:26:25 +0300

    [GenBank] Unite python scripts and correct the pipeline.
    The pipeline is broken now, load_missed.py content is not updated.
    Just make the draft of new pipeline structure.

--- NCBI_GenBank/genbank.sh
+++ NCBI_GenBank/genbank.sh
@@ -9,9 +9,9 @@ printf "ACvs source: $GDIR\n"
 date "+%D %T"
 
 printf "Downloading plasmids ...\n"
-time $WDIR/update_with_acv_list.py "$GDIR/plasmids.acvs" -s "$WDIR"
+time $WDIR/load_missed.py "$GDIR/plasmids.acvs" -s "$WDIR"
 
 printf "Downloading chromosomes ...\n"
-time $WDIR/update_with_acv_list.py "$GDIR/chromosomes.acvs" -s "$WDIR"
+time $WDIR/load_missed.py "$GDIR/chromosomes.acvs" -s "$WDIR"
 
 date "+%D %T"
