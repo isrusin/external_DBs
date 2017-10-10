52076b7d899ad44139186fb4c46ff3c34e555ad2
isrusin <is_rusinov@mail.ru>
2017-10-11 02:17:00 +0300

    [Taxonomy] Redirect messages to STDERR
    * Use python3 print function.
    * Redirect messages to STDERR.

--- Taxonomy/parse_taxdump.py
+++ Taxonomy/parse_taxdump.py
@@ -2,6 +2,7 @@
 
 """Parse NCBI taxdump archive and write data to TSV file."""
 
+from __future__ import print_function
 import argparse
 import sys
 import tarfile
@@ -31,28 +32,40 @@ def main(argv=None):
         if vals[3] == "scientific name":
             names[taxid] = name
     innames.close()
-    print "%d taxons were loaded." % len(taxids)
+    print("%d taxons were loaded." % len(taxids), file=sys.stderr)
     missed = len(taxids) - len(names)
     if missed:
-        print "%d taxids have no scientific names!" % missed
+        print(
+            "%d taxids have no scientific names!" % missed,
+            file=sys.stderr
+        )
     ranks = dict()
     innodes = intar.extractfile("nodes.dmp")
     for line in innodes:
         vals = line.strip("\n\t|").split("\t|\t")
         taxid, ptaxid, rank = vals[:3]
         if taxid not in taxids:
-            print "Warning: %s has no name at all!" % taxid
+            print(
+                "Warning: %s has no name at all!" % taxid,
+                file=sys.stderr
+            )
         if taxid not in names:
-            print "Warning: %s has no scientific name!" % taxid
+            print(
+                "Warning: %s has no scientific name!" % taxid,
+                file=sys.stderr
+            )
         ranks[taxid] = (rank, ptaxid)
     innodes.close()
-    print "%d nodes were loaded." % len(ranks)
+    print("%d nodes were loaded." % len(ranks), file=sys.stderr)
     with args.outab as outab:
-        outab.write("taxid\tparent_taxid\tscientific_name\trank\n")
+        outab.write("#:TaxID\tParent taxID\tScientific name\tRank\n")
         for taxid in taxids:
             name = names.get(taxid, "?")
             if taxid not in ranks:
-                print "%s has no rank and parent_taxid, skipped" % taxid
+                print(
+                    "%s has no rank and parent, skipped" % taxid,
+                    file=sys.stderr
+                )
                 continue
             rank, ptaxid = ranks[taxid]
             outab.write("\t".join((taxid, ptaxid, name, rank)) + "\n")
