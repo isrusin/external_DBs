c4dc14b89846e4538545148e1cd8cf9285a81687
isrusin <is_rusinov@mail.ru>
2017-10-11 00:08:36 +0300

    [Genome] Replace local genome IDs by Assembly ACs
    NCBI Assembly ACs turned to be unique for lines of 'prokaryotes.txt' file.
    Thus there is no need in locally generated genome IDs.
    
    Besides, add opportunity to work directly with raw 'prokaryotes.txt'
    without sorting. Add some checks in the code and additional columns
    into the output table for this purpose.

--- Genome/enumerate_genomes.py
+++ Genome/enumerate_genomes.py
@@ -29,29 +29,27 @@ def main(argv=None):
         help="tabular file with taxons"
     )
     parser.add_argument(
-        "-I", dest="idtag", metavar="STUB", default="{}",
-        help="ID stub, use {} as genome number placeholder; default '{}'"
-    )
-    parser.add_argument(
         "-o", dest="outab", metavar="FILE", type=argparse.FileType("w"),
         default=sys.stdout, help="output file, default STDOUT"
     )
     args = parser.parse_args(argv)
-    idtag = args.idtag
     taxons = load_taxons(args.intax)
     with args.intab as intab, args.outab as outab:
         columns = intab.readline().strip("\n#").split("\t")
         index = dict((col, ind) for ind, col in enumerate(columns))
         outab.write(
-            "Genome ID\tTaxID\tOrganism name\tChromosome ACs\t"
-            "Plasmid ACs\tOther ACs\tTaxonomy (rank|taxid|name) ...\n"
+            "#:Assembly AC\tTaxID\tOrganism name\tStatus\tWGS\t"
+            "Chromosomes\tPlasmids\tOther\tTaxonomy (rank|taxid|name)\n"
         )
         num = 0
         for line in intab:
             vals = line.strip().split("\t")
             name = vals[index["Organism/Name"]]
             taxid = vals[index["TaxID"]]
-            ouline = [idtag.format(num), taxid, name]
+            asac = vals[index["Assembly Accession"]]
+            status = vals[index["Status"]].split(" ")[-1]
+            wgs = vals[index["WGS"]].strip("-")
+            ouline = [asac, taxid, name, status, wgs]
             taxonomy = []
             while taxid in taxons:
                 parent, sname, rank = taxons[taxid]
@@ -59,26 +57,39 @@ def main(argv=None):
                     taxonomy.append("%s|%s|%s" % (rank, taxid, sname))
                 taxid = parent
             if not taxonomy:
-                print "%s has invalid taxID or no species, skipped!" % name
+                print (
+                    "Warning: assembly %s has invalid taxID "
+                    "or no species, skipped!"
+                ) % asac
                 continue
             chromosomes = set()
             plasmids = set()
             other = set()
-            for tag in vals[index["Replicons"]].split("; "):
-                seq_name, acv = tag.strip().split(":")
-                if seq_name.startswith("chromosome"):
-                    chromosomes.add(acv.strip("/"))
-                elif seq_name.startswith("plasmid"):
-                    plasmids.add(acv.strip("/"))
-                else:
-                    other.add(acv.strip("/"))
+            replicons = vals[index["Replicons"]]
+            if replicons != "-":
+                for tag in replicons.split("; "):
+                    if ":" not in tag:
+                        print (
+                            "Info: assembly %s contains replicon "
+                            "'%s' without sequence AC."
+                        ) % (asac, tag)
+                        continue
+                    seq_name, acv = tag.strip().split(":")
+                    if seq_name.startswith("chromosome"):
+                        chromosomes.add(acv.strip("/"))
+                    elif seq_name.startswith("plasmid"):
+                        plasmids.add(acv.strip("/"))
+                    else:
+                        other.add(acv.strip("/"))
+            else:
+                print "Info: assembly %s has no replicons." % asac
             ouline.append(",".join(sorted(chromosomes)))
             ouline.append(",".join(sorted(plasmids)))
             ouline.append(",".join(sorted(other)))
             ouline.extend(taxonomy)
             outab.write("\t".join(ouline) + "\n")
             num += 1
-    print "%d genomes were enumerated." % num
+    print "There are %d genomes in the resulted table." % num
 
 
 if __name__ == "__main__":
