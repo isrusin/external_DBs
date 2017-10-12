4cb7e8a42ca8327593e151579a4b2fdb5f83f23b
isrusin <is_rusinov@mail.ru>
2017-10-12 22:45:17 +0300

    [Genome] Change input format of filter_genomes
    Change input file format for filter_genomes.py from NCBI raw list to
    TSV obtained with add_taxonomy.py.
    
    * Add --with-species option.
    * Wrap message prints in a function.
    * Touch module docstring and description.

--- Genome/filter_genomes.py
+++ Genome/filter_genomes.py
@@ -1,10 +1,19 @@
 #! /usr/bin/env python
 
-"""Filter NCBI list of prokaryotic genomes by status, length, etc."""
+"""Filter a list of prokaryotic genomes by status, length, etc."""
 
 from __future__ import print_function
 import argparse
 import sys
+
+
+def message(msg, *args, **kwargs):
+    level = kwargs.pop("level", "info")
+    stream = kwargs.pop("stream", sys.stderr)
+    print(
+        "%s: %s" % (level.capitalize(), msg.format(*args, **kwargs)),
+        file=stream
+    )
 
 
 def main(argv=None):
@@ -12,15 +21,17 @@ def main(argv=None):
         description="Filter input list of genomes by status, length, etc."
     )
     parser.add_argument(
-        "intxt", metavar="FILE", type=argparse.FileType("r"),
-        help="prokaryotes.txt file from the NCBI FTP server"
+        "intsv", metavar="FILE", type=argparse.FileType("r"),
+        help="a list of genomes with taxonomy"
     )
     parser.add_argument(
         "status", metavar="STATUS", nargs="+",
-        help="status to keep, you could replace spaces with '_'"
+        choices={"Contig", "Scaffold", "Chromosome", "Genome"},
+        help="status to keep; any of 'Contig', 'Scaffold', 'Chromosome', "
+        "and 'Genome'"
     )
     parser.add_argument(
-        "-o", dest="outab", metavar="FILE", type=argparse.FileType("w"),
+        "-o", dest="outsv", metavar="FILE", type=argparse.FileType("w"),
         default=sys.stdout, help="output tabular file, default STDOUT"
     )
     parser.add_argument(
@@ -32,52 +43,39 @@ def main(argv=None):
         help="do not include genomes without chromosomes"
     )
     parser.add_argument(
+        "--with-species", dest="with_sp", action="store_true",
+        help="do not include genomes without species"
+    )
+    parser.add_argument(
         "--min-size", dest="min_size", metavar="SIZE", type=float,
         default=0, help="minimum genome size (Mb), default is 0 Mb"
     )
     args = parser.parse_args(argv)
-    status = set([tag.replace("_", " ") for tag in args.status])
-    with args.intxt as intxt, args.outab as outab:
-        title = intxt.readline()
-        columns = title.strip("\n#").split("\t")
+    statuses = set(args.keep_status)
+    with args.intsv as intsv, args.outsv as outsv:
+        title = intsv.readline()
+        outsv.write(title)
+        columns = title.strip("\t#: ").split("\t")
         c_index = dict([(col, ind) for ind, col in enumerate(columns)])
-        outab.write(title)
         for line in intxt:
             vals = line.strip().split("\t")
-            project = vals[c_index["BioProject Accession"]]
-            if vals[c_index["Status"]] not in status:
+            asac = vals[c_index["Assembly AC"]]
+            if vals[c_index["Status"]] not in statuses:
+                message("{} has bad status, skipped", asac)
                 continue
-            if args.with_chr:
-                repls = vals[c_index["Replicons"]]
-                if repls == "-":
-                    print(
-                        "%s has no replicons, skipped" % project,
-                        file=sys.stderr
-                    )
-                    continue
-                for repl_tag in repls.split(";"):
-                    seq_tag, acv_tag = repl_tag.strip().rsplit(":", 1)
-                    if seq_tag.startswith("chromosome"):
-                        break
-                else:
-                    print(
-                        "%s has no chromosomes, skipped" % project,
-                        file=sys.stderr
-                    )
-                    continue
-            if args.no_wgs and vals[c_index["WGS"]] != "-":
-                print(
-                    "%s has WGS accession, skipped" % project,
-                    file=sys.stderr
-                )
+            if args.with_chr and not vals[c_index["Chromosomes"]]:
+                message("{} has no chromosomes, skipped", asac)
                 continue
-            if float(vals[c_index["Size (Mb)"]]) < args.min_size:
-                print(
-                    "%s is too short, skipped" % project,
-                    file=sys.stderr
-                )
+            if args.with_sp and not vals[c_index["Species taxID"]]:
+                message("{} has no species, skipped", asac)
                 continue
-            outab.write(line)
+            if args.no_wgs and vals[c_index["WGS"]]:
+                message("{} has WGS accession, skipped", asac)
+                continue
+            if float(vals[c_index["Size, Mb"]]) < args.min_size:
+                message("{} is too short, skipped", asac)
+                continue
+            outsv.write(line)
 
 
 if __name__ == "__main__":
